454a6f885913c70375f0a14fdb458c6355a1a9f1
cbsim65535 <cbsim@zigbang.com>
2022-07-06 15:32:51 +0900

    u

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 var usbScanner = require('./usbscanner').usbScanner;
 var getDevices = require('./usbscanner').getDevices;
+
+const Gpio = require('pigpio').Gpio;
+
+const relay0 = new Gpio(5, { mode: Gpio.OUTPUT });
+const dry0 = new Gpio(6, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.EITHER_EDGE
+});
+
+
+dry0.on('interrupt', (level: any) => {
+    console.debug("dry0", level)
+});
+
 import { default as axios } from 'axios'
 
 const HASH = "kadsjflkajsdklfjakldsjlk2345786ed8adsjkfh2378346ehgdf7823hc7823h78cxh1238h"
@@ -15,13 +30,13 @@ var scanner = new usbScanner({ vendorId: 44176 });
 
 //scanner emits a data event once a barcode has been read and parsed
 scanner.on("data", function(code: string) {
-	console.debug("recieved code : " + code);
-	if (code.toUpperCase() == HASH.toUpperCase()) doorOpen()
+    console.debug("recieved code : " + code);
+    if (code.toUpperCase() == HASH.toUpperCase()) doorOpen()
 });
 
 function doorOpen() {
-	console.debug("DOOR OPEN")
-	axios.get(`http://192.168.0.223?p0=3000`)
+    console.debug("DOOR OPEN")
+    axios.get(`http://192.168.0.223?p0=3000`)
 
 }
 
