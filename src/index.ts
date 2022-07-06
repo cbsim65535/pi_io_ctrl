a2798d9a8e14cfd4a8c3a5afb43f007a49a02edb
cbsim65535 <cbsim@zigbang.com>
2022-07-06 15:36:48 +0900

    u

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ var getDevices = require('./usbscanner').getDevices;
 const Gpio = require('pigpio').Gpio;
 
 const relay0 = new Gpio(5, { mode: Gpio.OUTPUT });
+relay0.digitalWrite(0)
+
 const dry0 = new Gpio(6, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_DOWN,
@@ -26,13 +28,18 @@ var connectedHidDevices = getDevices()
 console.debug(connectedHidDevices)
 
 //initialize new usbScanner - takes optional parmeters vendorId and hidMap - check source for details
-var scanner = new usbScanner({ vendorId: 44176 });
+try {
+    var scanner = new usbScanner({ vendorId: 44176 });
 
-//scanner emits a data event once a barcode has been read and parsed
-scanner.on("data", function(code: string) {
-    console.debug("recieved code : " + code);
-    if (code.toUpperCase() == HASH.toUpperCase()) doorOpen()
-});
+    //scanner emits a data event once a barcode has been read and parsed
+    scanner.on("data", function(code: string) {
+        console.debug("recieved code : " + code);
+        if (code.toUpperCase() == HASH.toUpperCase()) doorOpen()
+    });
+}
+catch (e: any) {
+    console.error("not found qr reader")
+}
 
 function doorOpen() {
     console.debug("DOOR OPEN")
